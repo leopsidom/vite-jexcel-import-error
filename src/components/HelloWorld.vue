7bf22cfde4cd20505ab14ab2cea3d67af4150b78
Xiaoyun Yang <0.yxyun@gmail.com>
2021-01-16 13:18:23 -0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div ref="isheet"></div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, onMounted } from "vue";
+import jexcel from "jexcel";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  setup: () => {
+    const isheet = ref<HTMLDivElement | null>(null);
+    onMounted(() => {
+      if (isheet.value)
+        jexcel(isheet.value, {
+          minDimensions: [30, 10],
+        });
+    });
+    return { isheet };
+  },
+});
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
